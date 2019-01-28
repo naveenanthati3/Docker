b93c97660e3bb7620a4a051ad09dd06a7a7ef8f8
Naveen9devops <46016465+Naveen9devops@users.noreply.github.com>
2019-01-28 17:31:48 +0530

    adding walkhtrough

--- linking_containers/linking_containers.sh
+++ linking_containers/linking_containers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
+#pulling sql latest version and assigning password
 docker run --name our-mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
-
+#pulling adminer app and linking to sql and assigning port no
 docker run --link "our-mysql:db" -p 8080:8080 adminer
-
